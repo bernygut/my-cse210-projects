4a5a449c2949832166f8b51b42f3a4f8c4607549
bernygut <bernygutierrez@gmail.com>
2025-11-11 22:58:04 -0600

    Completed Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Fiserv";
+        job1._jobTitle = "Director of Security";
+        job1._startYear = 2009;
+        job1._endYear = 2021; 
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Senior Program Manager";
+        job2._startYear = 2021;
+        job2._endYear = 2100;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Berny Gutierrez";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
