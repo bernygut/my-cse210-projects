43aefeeb8a0a24cdc34aca251d089e2b674c3a6f
bernygut <bernygutierrez@gmail.com>
2025-11-21 22:26:16 -0600

    Completed Learning activity 3.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction FractionOne = new Fraction();
+        Console.WriteLine(FractionOne.GetFractionString());
+        Console.WriteLine(FractionOne.GetDecimalValue());
+
+        int whole = 5;
+        Fraction FractionTwo = new Fraction(whole);
+        Console.WriteLine(FractionTwo.GetFractionString());
+        Console.WriteLine(FractionTwo.GetDecimalValue());
+
+        int top1 = 3;
+        int bottom1 = 4;
+        Fraction FractionThree = new Fraction(top1, bottom1);
+        Console.WriteLine(FractionThree.GetFractionString());
+        Console.WriteLine(FractionThree.GetDecimalValue());
+
+        int top2 = 1;
+        int bottom2 = 3;
+        Fraction FractionFour = new Fraction(top2, bottom2);
+        Console.WriteLine(FractionFour.GetFractionString());
+        Console.WriteLine(FractionFour.GetDecimalValue());
+
     }
 }
